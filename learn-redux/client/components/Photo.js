7f9d77499c26927fd02576c8f7e7241d50ddfb9a
Daniel Cortes <daniel.cortes@rivetlogic.com>
2016-10-20 14:47:44 -0600

    Updating state with reducers

--- learn-redux/client/components/Photo.js
+++ learn-redux/client/components/Photo.js
@@ -22,7 +22,7 @@ const Photo = React.createClass({
         <figcaption>
           <p>{post.caption}</p>
           <div className="control-buttons">
-            <button className="likes">&hearts; {post.likes}</button>
+            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
             <Link to={`/view/${post.code}`} className="button">
               <span className="speech-bubble"></span>
               {comments[post.code] ? comments[post.code].length : 0}
